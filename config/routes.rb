66e7bd6faeaa4bc9547f7c44adf291d8ef78f463
Lota Plezere <lota.plezere@gmail.com>
2018-05-14 13:26:31 +0200

    question logic working

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,22 @@
 Rails.application.routes.draw do
+    get  '/ask', to: 'questions#ask'
+    get  '/answer', to: 'questions#answer'
+
+
+
+
+# Rails.application.routes.draw do
+#   # '/'
+#   root to: 'pages#home'
+
+#   # VERB  PATH          CONTROLLER#ACTION
+#     get  '/about', to: 'pages#about'
+#     get  '/contact', to: 'pages#contact'
+
+#   # restaurants                                       overriding the prefix
+#   get '/restaurants', to: 'restaurants#index'
+#   get '/restaurants/:id', to: 'restaurants#show', as: :restaurant
+#   post '/restaurants', to: 'restaurants#create'
+
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
